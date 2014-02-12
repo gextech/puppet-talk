4b26f430991b4701ea3ebb94cbbbde23aed30c80
atomsfat <atomsmail@gmail.com>
2014-02-12 16:48:07 -0600

    adding nodes for test

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-puppet-talk-
+puppet-talk
 ============
 
 puppet-talk for gex
+
+vagrant box add debian-wheezy http://169.254.187.171/~tomas/boxes/debian-wheezy-puppet.box 
+vagrant destroy
+vagrant up
+puppet apply
+java -version 
+puppet apply --verbose --modulepath '/etc/puppet/modules:/puppet-src/modules'  /puppet-src/init.pp --detailed-exitcodes || [ $? -eq 2 ]
+
+* Foreman is running at https://vagrant-debian-wheezy.vagrantup.com
+Default credentials are 'admin:changeme'
+* Foreman Proxy is running at https://vagrant-debian-wheezy.vagrantup.com:8443
+* Puppetmaster is running at port 8140
+
+
+vi /etc/puppet/puppet.conf
+
+server = vagrant-debian-wheezy.vagrantup.com
+report = true
+pluginsync = true
+certname = agent.example.local
+puppet agent --test
+
+on master
+puppet cert list
+puppet cert sign agent.example.local
+
+vi /etc/hosts
+10.177.62.203   puppet vagrant-debian-wheezy.vagrantup.com
+
+
+10.177.62.146
+
+
--- puppet-src/init.pp
+++ puppet-src/init.pp
@@ -1,1 +1,42 @@
-include gex
+Exec {
+  path => [
+    '/usr/local/bin',
+    '/opt/local/bin',
+    '/usr/bin', 
+    '/usr/sbin', 
+    '/bin',
+    '/sbin'],
+    logoutput => true,
+}
+exec {'echo 10.177.62.203 puppet vagrant-debian-wheezy.vagrantup.com >> /etc/hosts':}
+
+
+file{"/etc/puppet/puppet.conf":
+  path    => '/etc/puppet/puppet.conf',
+  content => "[main]
+  server = vagrant-debian-wheezy.vagrantup.com
+  report = true
+  pluginsync = true
+  certname = agent.vagrant.$domain
+  logdir=/var/log/puppet
+  vardir=/var/lib/puppet
+  ssldir=/var/lib/puppet/ssl
+  rundir=/var/run/puppet
+  factpath=\$vardir/lib/facter
+  templatedir=\$confdir/templates
+
+  [master]
+  # These are needed when the puppetmaster is run by passenger
+  # and can safely be removed if webrick is used.
+  ssl_client_header = SSL_CLIENT_S_DN
+  ssl_client_verify_header = SSL_CLIENT_VERIFY",
+  mode    => 0775,
+  ensure  => present
+}
+cron { puppetagent:
+ command => "puppet agent --test ",
+ user    => root,
+ hour    => "*",
+ minute  => "*/5",
+ require => File["/etc/puppet/puppet.conf"]
+}
